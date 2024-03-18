99682d13cc29a80e3c9b3caabf099ba42b4a4c8a
Sourav <souravsspace@gmail.com>
2024-03-19 03:58:14 +0600

    removed errors

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,6 @@
-import { getServerAuthSession } from "@/server/auth";
-import { redirect } from "next/navigation";
 import { type PropsWithChildren } from "react";
 
-export default async function AuthLayout({ children }: PropsWithChildren) {
-  const session = await getServerAuthSession();
-
-  if (session) {
-    redirect("/");
-  }
-
+export default function AuthLayout({ children }: PropsWithChildren) {
   return (
     <main className="mx-auto flex h-screen w-full items-center justify-center p-2 sm:p-4 md:p-6">
       {children}
